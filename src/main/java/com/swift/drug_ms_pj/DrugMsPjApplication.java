4c7b0bc75325f79f7f930bef6f581aa2c483a446
TransformersWsz <3287124026@qq.com>
2019-11-18 22:38:23 +0800

    init springboot+mybatis project

---
+++ src/main/java/com/swift/drug_ms_pj/DrugMsPjApplication.java
@@ -0,0 +1,13 @@
+package com.swift.drug_ms_pj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrugMsPjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrugMsPjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swift/drug_ms_pj/DrugMsPjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swift.drug_ms_pj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrugMsPjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
